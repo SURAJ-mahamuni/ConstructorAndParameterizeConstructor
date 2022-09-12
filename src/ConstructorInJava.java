e6b25bc093c65dc9345dc763a95d5fa240c1a239
Suraj <surajmaha00@gmail.com>
2022-09-12 17:59:18 +0530

    parameterize constructor and non-parameterize constructor

---
+++ src/ConstructorInJava.java
@@ -0,0 +1,9 @@
+public class ConstructorInJava {
+    //public constructor
+    public ConstructorInJava() {
+        System.out.println("This a non-parameterize constructor");
+    }
+    public ConstructorInJava(int num){    //int num is integer parameter of this constructor is accepte one integer
+        System.out.println(num);
+    }
+}
